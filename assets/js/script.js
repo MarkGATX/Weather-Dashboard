7561cb2fc3b6b5d9fb309c510d4626b6a342b0d7
Mark Gardner <themarkgardner@gmail.com>
2022-10-10 01:12:40 -0500

    added animations to forecast information with timerintervals and styles. Fixed bug for cities that didn't return any values with a modal and clearing the input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// slide element
+let forecastPane = document.querySelector('.slide');
+console.log(forecastPane)
+
+
+
 // global variables
 var citySearch = document.querySelector('#citySearchName');
 var cityListParent = document.querySelector('#possibleCityList');
@@ -38,13 +44,15 @@ pastSearchParent.addEventListener("click", pullLocalStorage)
 
 function pullLocalStorage(event) {
     event.preventDefault();
+    forecastPane.style.transform = "translateX(1000px)";
     cityName = event.target.textContent;
     console.log(cityName)
     lastFiveCity = JSON.parse(localStorage.getItem("lastFiveCityLocal"));
     for (let i = 0; i < lastFiveCity.length; i++) {
         if (cityName === lastFiveCity[i][0]) {
             cityLatLongURL = lastFiveCity[i][1];
-            latLongWeatherRequest();
+            setTimeout(latLongWeatherRequest, 1000)
+
             return
         }
     }
@@ -54,6 +62,7 @@ function pullLocalStorage(event) {
 //call weatherapi and find city and weather
 function searchCity(event) {
     event.preventDefault();
+    forecastPane.style.transform = "translateX(1000px)";
     city = citySearch.value.trim();
     var cityArray = city.split(',');
     var cityURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + weatherAPI;
@@ -63,6 +72,12 @@ function searchCity(event) {
         })
         .then(function (data) {
             console.log(data)
+            if (data.length === 0) {
+                $('#noResultsWarning').modal();
+                citySearch.value = "";
+
+                return;
+            }
             if (data.length > 1) {
                 possibleCityList = data;
                 chooseCity(data);
@@ -70,156 +85,159 @@ function searchCity(event) {
                 lat = data[0].lat;
                 long = data[0].lon;
                 latLongWeatherRequest();
- 
+
             };
         });
+}
+
+
+
+//Populate choice for multiple cities
+function chooseCity(data) {
+    // Build city list after clearing field first 
+    cityListParent.innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+        var cityListEl = document.createElement('li');
+        cityListEl.textContent = data[i].name + ", " + data[i].state;
+        console.log(cityListEl);
+        console.log(cityListParent)
+        cityListParent.appendChild(cityListEl);
+        cityListParent.addEventListener("click", getLatLong);
     }
+    $('#citySelection').modal()
 
-            //Populate choice for multiple cities
-            function chooseCity(data) {
-                // Build city list after clearing field first 
-                cityListParent.innerHTML = '';
-                for (let i = 0; i < data.length; i++) {
-                    var cityListEl = document.createElement('li');
-                    cityListEl.textContent = data[i].name + ", " + data[i].state;
-                    console.log(cityListEl);
-                    console.log(cityListParent)
-                    cityListParent.appendChild(cityListEl);
-                    cityListParent.addEventListener("click", getLatLong);
+}
+
+//get latitude and longitude from chosen city
+function getLatLong(city) {
+    cityName = city.target.textContent;
+    $('#citySelection').modal('hide');
+    //cycle through array of cities for a match then pull lat and long
+    for (i = 0; i < possibleCityList.length; i++) {
+        if (cityName === possibleCityList[i].name + ", " + possibleCityList[i].state) {
+            lat = possibleCityList[i].lat;
+            long = possibleCityList[i].lon
+            cityLatLongURL = 'https://api.weatherbit.io/v2.0/forecast/daily?lat=' + lat + '&lon=' + long + '&key=' + weatherBitAPI;
+            latLongWeatherRequest();
+            return;
+        }
+    }
+    console.log(cityName);
+}
+
+//submit weather request with lat and long and store new local array
+function latLongWeatherRequest() {
+    fetch(cityLatLongURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //check to see if duplicate name, 
+            let dataCityNameCap = data.city_name;
+            dataCityNameCap.toUpperCase();
+            for (let i = 0; i < lastFiveCity.length; i++) {
+                if (data.city_name.toUpperCase() === lastFiveCity[i][0].toUpperCase()) {
+                    var dupeCity = true;
+                    break;
                 }
-                $('#citySelection').modal()
-
+            }
+            // if  duplicate name, fill out forecast
+            if (dupeCity === true) {
+                popWeatherForecast(data);
+                return;
             }
 
-            //get latitude and longitude from chosen city
-            function getLatLong(city) {
-                cityName = city.target.textContent;
-                $('#citySelection').modal('hide');
-                //cycle through array of cities for a match then pull lat and long
-                for (i = 0; i < possibleCityList.length; i++) {
-                    if (cityName === possibleCityList[i].name + ", " + possibleCityList[i].state) {
-                        lat = possibleCityList[i].lat;
-                        long = possibleCityList[i].lon
-                        cityLatLongURL = 'https://api.weatherbit.io/v2.0/forecast/daily?lat=' + lat + '&lon=' + long + '&key=' + weatherBitAPI;
-                        latLongWeatherRequest();
-                        return;
-                    }
-                }
-                console.log(cityName);
+            //if no dupe, add to array
+            lastFiveCity.unshift([data.city_name, cityLatLongURL]);
+
+            // if array > 5 then remove the last value
+            if (lastFiveCity.length > 5) {
+                lastFiveCity.pop();
             }
 
-            //submit weather request with lat and long and store new local array
-            function latLongWeatherRequest() {
-                fetch(cityLatLongURL)
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        //check to see if duplicate name, 
-                        let dataCityNameCap = data.city_name;
-                        dataCityNameCap.toUpperCase();
-                        for (let i = 0; i < lastFiveCity.length; i++) {
-                            if (data.city_name.toUpperCase() === lastFiveCity[i][0].toUpperCase()) {
-                                var dupeCity = true;
-                                break;
-                            }
-                        }
-                        // if  duplicate name, fill out forecast
-                        if (dupeCity === true) {
-                            popWeatherForecast(data);
-                            return;
-                        }
-
-                        //if no dupe, add to array
-                        lastFiveCity.unshift([data.city_name, cityLatLongURL]);
-
-                        // if array > 5 then remove the last value
-                        if (lastFiveCity.length > 5) {
-                            lastFiveCity.pop();
-                        }
-
-                        //add previous search button
-                        localStorage.setItem('lastFiveCityLocal', JSON.stringify(lastFiveCity));
-                        var pastSearchParent = document.querySelector('#pastSearches');
-                        var pastButton = document.createElement('button');
-                        pastButton.textContent = data.city_name;
-                        pastButton.classList.add('w-100', 'pastSearchBtn', 'text-light', 'mb-2', 'p-2', 'rounded', 'font-weight-bold');
-                        var buttonListParent = document.getElementById('pastSearches');
-                        console.log(buttonListParent.childElementCount)
-                        //if at least 1 child li, add the next at the beginning of the list
-                        if (buttonListParent.childElementCount > 0) {
-                            pastSearchParent.insertBefore(pastButton, pastSearchParent.firstChild);
-                        } else {
-                            pastSearchParent.appendChild(pastButton)
-                        }
-                        //remove button greater than 5 under search input
-                        console.log(buttonListParent.childElementCount)
-                        if (buttonListParent.childElementCount > 5) {
-                            buttonListParent.removeChild(buttonListParent.lastChild);
-                        }
-                        popWeatherForecast(data);
-                    })
+            //add previous search button
+            localStorage.setItem('lastFiveCityLocal', JSON.stringify(lastFiveCity));
+            var pastSearchParent = document.querySelector('#pastSearches');
+            var pastButton = document.createElement('button');
+            pastButton.textContent = data.city_name;
+            pastButton.classList.add('w-100', 'pastSearchBtn', 'text-light', 'mb-2', 'p-2', 'rounded', 'font-weight-bold');
+            var buttonListParent = document.getElementById('pastSearches');
+            console.log(buttonListParent.childElementCount)
+            //if at least 1 child li, add the next at the beginning of the list
+            if (buttonListParent.childElementCount > 0) {
+                pastSearchParent.insertBefore(pastButton, pastSearchParent.firstChild);
+            } else {
+                pastSearchParent.appendChild(pastButton)
             }
-
-
-            function popWeatherForecast(data) {
-                // Use weatherBit data for daily forecast
-                for (let i = 0; i < 6; i++) {
-                    var finalCityName = data.city_name
-                    let date = moment(data.data[i].ts, 'X').format('dddd, MMMM Do YYYY');
-                    let currentTemp = ((Math.round(data.data[i].temp)) * 9 / 5) + 32;
-                    let minTemp = ((Math.round(data.data[i].low_temp)) * 9 / 5) + 32;
-                    let maxTemp = ((Math.round(data.data[i].high_temp)) * 9 / 5) + 32;
-                    let humidity = data.data[i].rh;
-                    let windSpeed = (Math.round(data.data[i].wind_spd)) * 2.237;
-                    let windGust = (Math.round(data.data[i].wind_gust_spd)) * 2.237;
-                    let chanceOfRain = (data.data[i].pop);
-                    let conditions = data.data[i].weather.description;
-                    let conditionsIcon = data.data[i].weather.icon;
-                    let dateShort = moment(data.data[i].ts, 'X').format('ddd, MMM Do');
-                    if (i === 0) {
-                        document.getElementById('todayDate').innerHTML = finalCityName + " on " + date + ".  <img src=" + "'https://www.weatherbit.io/static/img/icons/" + conditionsIcon + ".png' alt='weather conditions'>";
-                        document.getElementById('weatherDesc').textContent = conditions + " and " + currentTemp + "° fahrenheit. There's currently a " + chanceOfRain + "% chance of rain.";
-                        document.getElementById('windDesc').textContent = "Winds are at " + windSpeed + " MPH with gusts up to " + windGust + " MPH.";
-                        document.getElementById('highsLows').innerHTML = "";
-                        var minLi = document.createElement('li');
-                        minLi.innerHTML = "<strong>Nighttime Low:</strong>  " + minTemp + "° ";
-                        document.getElementById('highsLows').appendChild(minLi);
-                        var maxLi = document.createElement('li');
-                        maxLi.innerHTML = "<strong>Daytime High:</strong>  " + maxTemp + "° ";
-                        document.getElementById('highsLows').appendChild(maxLi);
-                        var humid = document.createElement('li');
-                        humid.innerHTML = "<strong>Humidity:</strong>  " + humidity + "%";
-                        document.getElementById('highsLows').appendChild(humid);
-
-
-                    } else {
-                        var mainCard = document.getElementById(i);
-                        var fiveDayCardTitle = mainCard.querySelector('.card-title')
-                        fiveDayCardTitle.textContent = dateShort;
-                        mainCard.querySelector('.card-text').innerHTML = "<img src=" + "'https://www.weatherbit.io/static/img/icons/" + conditionsIcon + ".png' alt='weather conditions'>";
-                        var forecastDayHigh = document.createElement('li');
-                        var forecastNightLow = document.createElement('li');
-                        var forecastHumid = document.createElement('li');
-                        var forecastRain = document.createElement('li');
-                        var forecastWind = document.createElement('li');
-                        var parentUL = document.getElementById(i);
-                        console.log(parentUL.nextElementSibling)
-                        parentUL.nextElementSibling.innerHTML = "";
-                        forecastDayHigh.textContent = 'High: ' + (((Math.round(data.data[i].high_temp)) * 9 / 5) + 32) + '°'
-                        forecastNightLow.textContent = 'Low: ' + (((Math.round(data.data[i].low_temp)) * 9 / 5) + 32) + '°'
-                        forecastRain.textContent = 'Rain%: ' + (data.data[i].pop) + '%'
-                        forecastHumid.textContent = 'Humidity: ' + data.data[i].rh + '°'
-                        forecastWind.textContent = 'Winds: ' + Math.round(data.data[i].wind_spd) + ' MPH'
-                        parentUL.nextElementSibling.appendChild(forecastDayHigh);
-                        parentUL.nextElementSibling.appendChild(forecastNightLow);
-                        parentUL.nextElementSibling.appendChild(forecastRain);
-                        parentUL.nextElementSibling.appendChild(forecastHumid);
-                        parentUL.nextElementSibling.appendChild(forecastWind);
-                    }
-                }
-                return
+            //remove button greater than 5 under search input
+            console.log(buttonListParent.childElementCount)
+            if (buttonListParent.childElementCount > 5) {
+                buttonListParent.removeChild(buttonListParent.lastChild);
             }
+            popWeatherForecast(data);
+        })
+}
+
+
+function popWeatherForecast(data) {
+    // Use weatherBit data for daily forecast
+    for (let i = 0; i < 6; i++) {
+        var finalCityName = data.city_name
+        let date = moment(data.data[i].ts, 'X').format('dddd, MMMM Do YYYY');
+        let currentTemp = ((Math.round(data.data[i].temp)) * 9 / 5) + 32;
+        let minTemp = ((Math.round(data.data[i].low_temp)) * 9 / 5) + 32;
+        let maxTemp = ((Math.round(data.data[i].high_temp)) * 9 / 5) + 32;
+        let humidity = data.data[i].rh;
+        let windSpeed = (Math.round(data.data[i].wind_spd)) * 2.237;
+        let windGust = (Math.round(data.data[i].wind_gust_spd)) * 2.237;
+        let chanceOfRain = (data.data[i].pop);
+        let conditions = data.data[i].weather.description;
+        let conditionsIcon = data.data[i].weather.icon;
+        let dateShort = moment(data.data[i].ts, 'X').format('ddd, MMM Do');
+        if (i === 0) {
+            document.getElementById('todayDate').innerHTML = finalCityName + " on " + date + ".  <img src=" + "'https://www.weatherbit.io/static/img/icons/" + conditionsIcon + ".png' alt='weather conditions'>";
+            document.getElementById('weatherDesc').textContent = conditions + " and " + currentTemp + "° fahrenheit. There's currently a " + chanceOfRain + "% chance of rain.";
+            document.getElementById('windDesc').textContent = "Winds are at " + windSpeed + " MPH with gusts up to " + windGust + " MPH.";
+            document.getElementById('highsLows').innerHTML = "";
+            var minLi = document.createElement('li');
+            minLi.innerHTML = "<strong>Nighttime Low:</strong>  " + minTemp + "° ";
+            document.getElementById('highsLows').appendChild(minLi);
+            var maxLi = document.createElement('li');
+            maxLi.innerHTML = "<strong>Daytime High:</strong>  " + maxTemp + "° ";
+            document.getElementById('highsLows').appendChild(maxLi);
+            var humid = document.createElement('li');
+            humid.innerHTML = "<strong>Humidity:</strong>  " + humidity + "%";
+            document.getElementById('highsLows').appendChild(humid);
+
+
+        } else {
+            var mainCard = document.getElementById(i);
+            var fiveDayCardTitle = mainCard.querySelector('.card-title')
+            fiveDayCardTitle.textContent = dateShort;
+            mainCard.querySelector('.card-text').innerHTML = "<img src=" + "'https://www.weatherbit.io/static/img/icons/" + conditionsIcon + ".png' alt='weather conditions'>";
+            var forecastDayHigh = document.createElement('li');
+            var forecastNightLow = document.createElement('li');
+            var forecastHumid = document.createElement('li');
+            var forecastRain = document.createElement('li');
+            var forecastWind = document.createElement('li');
+            var parentUL = document.getElementById(i);
+            console.log(parentUL.nextElementSibling)
+            parentUL.nextElementSibling.innerHTML = "";
+            forecastDayHigh.textContent = 'High: ' + (((Math.round(data.data[i].high_temp)) * 9 / 5) + 32) + '°'
+            forecastNightLow.textContent = 'Low: ' + (((Math.round(data.data[i].low_temp)) * 9 / 5) + 32) + '°'
+            forecastRain.textContent = 'Rain%: ' + (data.data[i].pop) + '%'
+            forecastHumid.textContent = 'Humidity: ' + data.data[i].rh + '°'
+            forecastWind.textContent = 'Winds: ' + Math.round(data.data[i].wind_spd) + ' MPH'
+            parentUL.nextElementSibling.appendChild(forecastDayHigh);
+            parentUL.nextElementSibling.appendChild(forecastNightLow);
+            parentUL.nextElementSibling.appendChild(forecastRain);
+            parentUL.nextElementSibling.appendChild(forecastHumid);
+            parentUL.nextElementSibling.appendChild(forecastWind);
+        }
+    }
+    forecastPane.style.transform = "translateX(0)";
+    return
+}
 
 
                 //POPULATE THE CURRENT DAY WEATHER
